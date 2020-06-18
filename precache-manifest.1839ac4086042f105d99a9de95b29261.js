180fc7c2bfacbcc4c44f265aa0219a5867d0f079
Ravi do Valle Luz <ravi.luz@usp.br>
2020-06-18 16:26:51 -0300

    Updates

---
+++ precache-manifest.1839ac4086042f105d99a9de95b29261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30ebcc4d92f2d298eb4041cb423f84c",
+    "url": "/Vendor-Product-Frontend/index.html"
+  },
+  {
+    "revision": "3199a82044204fa0930c",
+    "url": "/Vendor-Product-Frontend/static/css/main.ffba3b6a.chunk.css"
+  },
+  {
+    "revision": "0c6cb80fcdd098ec8245",
+    "url": "/Vendor-Product-Frontend/static/js/2.96dc2566.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Vendor-Product-Frontend/static/js/2.96dc2566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3199a82044204fa0930c",
+    "url": "/Vendor-Product-Frontend/static/js/main.63f8096e.chunk.js"
+  },
+  {
+    "revision": "8cfc3eedcacd36de9b81",
+    "url": "/Vendor-Product-Frontend/static/js/runtime-main.60d0958d.js"
+  }
+]);
